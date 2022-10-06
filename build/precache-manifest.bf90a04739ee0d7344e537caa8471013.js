7c25883ffa0500409c170c0f9804986cb37b6988
Are12-7 <carlos.andres97.ca@gmail.com>
2022-10-06 16:40:22 -0400

    React project code added

---
+++ build/precache-manifest.bf90a04739ee0d7344e537caa8471013.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d23f80283c6b01f797c",
+    "url": "/Are12-7/COMP3104-LAB5/static/css/main.ecb35ef0.chunk.css"
+  },
+  {
+    "revision": "9d23f80283c6b01f797c",
+    "url": "/Are12-7/COMP3104-LAB5/static/js/main.9d23f802.chunk.js"
+  },
+  {
+    "revision": "0957294d3c176abd13bb",
+    "url": "/Are12-7/COMP3104-LAB5/static/js/runtime~main.0957294d.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Are12-7/COMP3104-LAB5/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "845d8451a83acbe329ac3088cd7240f4",
+    "url": "/Are12-7/COMP3104-LAB5/index.html"
+  }
+];
